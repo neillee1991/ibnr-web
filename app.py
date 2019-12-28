9faaa86245400f4a5dae7d14bff3f83136f6ab12
LiSheng <neillee1991@gmail.com>
2019-12-28 17:11:49 +0800

    function complete
    function complete

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 from flask import Flask
 from flask import render_template
-from flask import Flask,flash,render_template,request,redirect,jsonify
+from flask import Flask, flash, render_template, request, redirect, jsonify
 from werkzeug.utils import secure_filename
 import os
 from ibnr_cal import *
 
-
-
-
 UPLOAD_FOLDER = '.\\tmp'
-ALLOWED_EXTENSIONS = set(['xlsx','csv','xls'])
+ALLOWED_EXTENSIONS = set(['xlsx', 'csv', 'xls'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.debug=True
+app.debug = True
 app.config['SECRET_KEY'] = '123456'
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,12 +20,10 @@ def index():
     return render_template('index.html')
 
 
-
-
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     print('upload')
+
     def allowed_file(filename):
         return '.' in filename and \
                filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -45,31 +39,104 @@ def upload():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(path)
 
-            returndata=read_data(path)
+            returndata = read_data(path)
             return jsonify(returndata)
     return ''
 
+
 @app.route('/caltri', methods=['GET', 'POST'])
 def caltri():
-    print(caltri)
+    print('caltri')
+    import json
+    data = json.loads(request.get_data())
+    filename = data['filename']
+    date_start = data['date_start']
+    date_end = data['date_end']
+
+    local_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    tables = cal_tri(local_path, date_start, date_end)
+    ret = ''
+    for t in tables:
+        ret += t
+    return ret
 
 
-    import pdb
-    pdb.set_trace()
+@app.route('/calfactor', methods=['GET', 'POST'])
+def calfactor():
+    print('calfactor')
+    import json
+    data = json.loads(request.get_data())
+    filename = data['filename']
+    date_start = data['date_start']
+    date_end = data['date_end']
+    cfactor_month_num = data['cfactor_month_num']
+    ffactor_month_num = data['ffactor_month_num']
+    local_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    tables = cal_factor(local_path, date_start, date_end, cfactor_month_num, ffactor_month_num)
+    ret = ''
+    for t in tables:
+        ret += t
+    return ret
 
 
-    print(request.json)
-    filename = request.data['filename']
-    start_date=request.data['start_date']
-    end_date=request.data['end_date']
+@app.route('/calres', methods=['GET', 'POST'])
+def calres():
+    print('calres')
+    import json
+    data = json.loads(request.get_data())
+    filename = data['filename']
+    date_start = data['date_start']
+    date_end = data['date_end']
+    cfactor_adj = data['cfactor_adj']
+    ffactor_adj = data['ffactor_adj']
+    local_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    tables = cal_res(local_path, date_start, date_end, cfactor_adj, ffactor_adj)
 
-    local_path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    print(local_path,start_date,end_date)
+    ret = ''
+    for t in tables:
+        ret += t
+    return ret
 
 
+@app.route('/method2', methods=['GET', 'POST'])
+def method2():
+    print('method2')
+    import json
+    data = json.loads(request.get_data())
+    filename = data['filename']
+    date_start = data['date_start']
+    date_end = data['date_end']
+    af_month_num = data['af_month_num']
+
+    local_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    tables = method_2(local_path, date_start, date_end, af_month_num)
+
+    ret = ''
+    for t in tables:
+        ret += t
+    return ret
+
+
+@app.route('/method2res', methods=['GET', 'POST'])
+def method2res():
+    print('method2res')
+    import json
+    data = json.loads(request.get_data())
+    filename = data['filename']
+    date_start = data['date_start']
+    date_end = data['date_end']
+    average_fee_adj = data['average_fee_adj']
+    cfactor_adj = data['cfactor_adj']
+    local_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    tables = method_2_res(local_path, date_start, date_end, cfactor_adj, average_fee_adj)
+
+    ret = ''
+    for t in tables:
+        ret += t
+    return ret
 
 
 if __name__ == '__main__':
--- ibnr_cal.py
+++ ibnr_cal.py
@@ -3,34 +3,219 @@ from pandas import DataFrame
 
 
 def read_data(path):
-    if path.split(".")[-1] in ['xlsx','xls']:
-        data=pd.read_excel(path)
+    if path.split(".")[-1] in ['xlsx', 'xls']:
+        data = pd.read_excel(path)
     else:
-        data=pd.read_csv(path,engine='python',encoding='gbk')
+        data = pd.read_csv(path, engine='python', encoding='gbk')
     import datetime
 
     data['occur_month'] = data['occur_month'].map(lambda x: datetime.datetime.strftime(x, '%Y年%m月'))
     data['acc_close'] = data['acc_close'].map(lambda x: ('%.0f') % x)
     data['cntr_year'] = data['cntr_year'].map(lambda x: ('%.0f') % x)
-    data['pay_amnt']=data['pay_amnt'].map(lambda x: ('%.2f') % x)
+    data['pay_amnt'] = data['pay_amnt'].map(lambda x: ('%.2f') % x)
 
-    ret_dict={}
-    ret_dict['shapex']=data.shape[0]
+    ret_dict = {}
+    ret_dict['shapex'] = data.shape[0]
     ret_dict['shapey'] = data.shape[1]
     ret_dict['head_table'] = data.head(20).to_dict(orient='records')
-    ret_dict['datemax']=data['occur_month'].max()
+    ret_dict['datemax'] = data['occur_month'].max()
     ret_dict['datemin'] = data['occur_month'].min()
 
     counts = data['cntr_year'].value_counts()
     tmp = DataFrame([counts.index.tolist(), counts.values.tolist()]).T
     tmp.columns = ['cntr_year', 'counts']
-    ret_dict['cntr_years']=tmp.to_dict(orient='record')
+    ret_dict['cntr_years'] = tmp.to_dict(orient='record')
     return ret_dict
 
 
+def cal_tri(local_path, start_date, end_date):
+    import datetime
+    start = datetime.datetime.strptime(start_date, '%Y年%m月')
+    end = datetime.datetime.strptime(end_date, '%Y年%m月')
+    if local_path.split(".")[-1] in ['xlsx', 'xls']:
+        data = pd.read_excel(local_path)
+    else:
+        data = pd.read_csv(local_path, engine='python', encoding='gbk')
+
+    df = data[(data['occur_month'] >= start) & (data['occur_month'] <= end)]
+    df['count'] = 1
+    _, ct = get_triangle(df, 'occur_month', 'acc_close', 'count', sum)
+    _, ft = get_triangle(df, 'occur_month', 'acc_close', 'pay_amnt', sum)
+    acc_ct = get_acc_triangle(ct)
+    acc_ft = get_acc_triangle(ft)
+
+    ct_html = ct.to_html(header="true", table_id="ct", classes='table table-condensed', float_format='%.0f')
+    acc_ct_html = acc_ct.to_html(header="true", table_id="acc_ct", classes='table table-condensed', float_format='%.0f')
+    ft_html = ft.to_html(header="true", table_id="ft", classes='table table-condensed', float_format='%.0f')
+    acc_ft_html = acc_ft.to_html(header="true", table_id="acc_ft", classes='table table-condensed', float_format='%.0f')
+
+    tables = [ct_html, acc_ct_html, ft_html, acc_ft_html]
+    return tables
 
 
+def cal_factor(local_path, start_date, end_date, cfactor_month_num, ffactor_month_num):
+    import datetime
+    start = datetime.datetime.strptime(start_date, '%Y年%m月')
+    end = datetime.datetime.strptime(end_date, '%Y年%m月')
+    if local_path.split(".")[-1] in ['xlsx', 'xls']:
+        data = pd.read_excel(local_path)
+    else:
+        data = pd.read_csv(local_path, engine='python', encoding='gbk')
 
+    df = data[(data['occur_month'] >= start) & (data['occur_month'] <= end)]
+    df['count'] = 1
+    _, ct = get_triangle(df, 'occur_month', 'acc_close', 'count', sum)
+    _, ft = get_triangle(df, 'occur_month', 'acc_close', 'pay_amnt', sum)
+    acc_ct = get_acc_triangle(ct)
+    acc_ft = get_acc_triangle(ft)
+    if cfactor_month_num in ['', 'None', 'none', 'NONE']:
+        cfactor_month_num = None
+    else:
+        cfactor_month_num = int(cfactor_month_num)
+
+    if ffactor_month_num in ['', 'None', 'none', 'NONE']:
+        ffactor_month_num = None
+    else:
+        ffactor_month_num = int(ffactor_month_num)
+
+    c_factor = get_factor(acc_ct, factor_month_num=cfactor_month_num)
+    f_factor = get_factor(acc_ft, factor_month_num=ffactor_month_num)
+
+    ret_c_factor = DataFrame([c_factor], columns=range(len(c_factor)), index=['件数']).to_html(header="true",
+                                                                                             table_id="ret_c_factor",
+                                                                                             classes='table table-condensed',
+                                                                                             float_format='%.4f')
+    ret_f_factor = DataFrame([f_factor], columns=range(len(f_factor)), index=['费用']).to_html(header="true",
+                                                                                             table_id="ret_f_factor",
+                                                                                             classes='table table-condensed',
+                                                                                             float_format='%.4f')
+
+    tables = [ret_c_factor, ret_f_factor]
+    return tables
+
+
+def cal_res(local_path, start_date, end_date, cfactor_adj, ffactor_adj):
+    import datetime
+    start = datetime.datetime.strptime(start_date, '%Y年%m月')
+    end = datetime.datetime.strptime(end_date, '%Y年%m月')
+    if local_path.split(".")[-1] in ['xlsx', 'xls']:
+        data = pd.read_excel(local_path)
+    else:
+        data = pd.read_csv(local_path, engine='python', encoding='gbk')
+
+    df = data[(data['occur_month'] >= start) & (data['occur_month'] <= end)]
+    df['count'] = 1
+    _, ct = get_triangle(df, 'occur_month', 'acc_close', 'count', sum)
+    _, ft = get_triangle(df, 'occur_month', 'acc_close', 'pay_amnt', sum)
+    acc_ct = get_acc_triangle(ct)
+    acc_ft = get_acc_triangle(ft)
+    ct_f = list(map(lambda x: 1 if x in ['', 'None', 'none', 'NONE'] else float(x), cfactor_adj.split('\t')))
+    ft_f = list(map(lambda x: 1 if x in ['', 'None', 'none', 'NONE'] else float(x), ffactor_adj.split('\t')))
+    est_ct, est_acc_ct = get_est_triangle(ct, acc_ct, ct_f)
+    est_ft, est_acc_ft = get_est_triangle(ft, acc_ft, ft_f)
+
+    count_ibnr = get_ibnr(est_acc_ct)
+    fee_ibnr = get_ibnr(est_acc_ft)
+
+    ret_count_ibnr = DataFrame([count_ibnr], columns=range(len(count_ibnr)),
+                               index=['件数']).to_html(header="true", table_id="ret_c_factor",
+                                                     classes='table table-condensed', float_format='%.4f')
+    ret_fee_ibnr = DataFrame([fee_ibnr], columns=range(len(fee_ibnr)),
+                             index=['费用']).to_html(header="true", table_id="ret_f_factor",
+                                                   classes='table table-condensed', float_format='%.4f')
+
+    est_ct_html = est_ct.to_html(header="true", table_id="est_ct", classes='table table-condensed', float_format='%.0f')
+    est_acc_ct_html = est_acc_ct.to_html(header="true", table_id="est_acc_ct", classes='table table-condensed',
+                                         float_format='%.0f')
+    est_ft_html = est_ft.to_html(header="true", table_id="est_ft", classes='table table-condensed', float_format='%.0f')
+    est_acc_ft_html = est_acc_ft.to_html(header="true", table_id="est_acc_ft", classes='table table-condensed',
+                                         float_format='%.0f')
+
+    tables = [ret_count_ibnr, ret_fee_ibnr, est_ct_html, est_acc_ct_html, est_ft_html, est_acc_ft_html]
+    return tables
+
+
+def method_2(local_path, start_date, end_date, af_month_num):
+    import datetime
+    start = datetime.datetime.strptime(start_date, '%Y年%m月')
+    end = datetime.datetime.strptime(end_date, '%Y年%m月')
+    if local_path.split(".")[-1] in ['xlsx', 'xls']:
+        data = pd.read_excel(local_path)
+    else:
+        data = pd.read_csv(local_path, engine='python', encoding='gbk')
+
+    df = data[(data['occur_month'] >= start) & (data['occur_month'] <= end)]
+    df['count'] = 1
+    _, ct = get_triangle(df, 'occur_month', 'acc_close', 'count', sum)
+    _, ft = get_triangle(df, 'occur_month', 'acc_close', 'pay_amnt', sum)
+
+    if af_month_num in ['', 'None', 'none', 'NONE']:
+        af_month_num = None
+    else:
+        af_month_num = int(af_month_num)
+
+    average_fee, average_fee_triangle = get_average_fee(ct, ft, af_month_num)
+
+    average_fee_html = DataFrame([average_fee], columns=range(len(average_fee)), index=['平均费用']).to_html(header="true",
+                                                                                                         table_id="average_fee",
+                                                                                                         classes='table table-condensed',
+                                                                                                         float_format='%.0f')
+    average_fee_triangle_html = average_fee_triangle.to_html(header="true",
+                                                             table_id="average_fee_triangle_html",
+                                                             classes='table table-condensed',
+                                                             float_format='%.0f')
+    tables = [average_fee_html, average_fee_triangle_html]
+    return tables
+
+
+def method_2_res(local_path, start_date, end_date, cfactor_adj, average_fee_adj):
+    import datetime
+    start = datetime.datetime.strptime(start_date, '%Y年%m月')
+    end = datetime.datetime.strptime(end_date, '%Y年%m月')
+    if local_path.split(".")[-1] in ['xlsx', 'xls']:
+        data = pd.read_excel(local_path)
+    else:
+        data = pd.read_csv(local_path, engine='python', encoding='gbk')
+
+    df = data[(data['occur_month'] >= start) & (data['occur_month'] <= end)]
+    df['count'] = 1
+    _, ct = get_triangle(df, 'occur_month', 'acc_close', 'count', sum)
+    _, ft = get_triangle(df, 'occur_month', 'acc_close', 'pay_amnt', sum)
+    acc_ct = get_acc_triangle(ct)
+
+    ct_f = list(map(lambda x: 1 if x in ['', 'None', 'none', 'NONE'] else float(x), cfactor_adj.split('\t')))
+
+    f_list = average_fee_adj.split('\t')
+    af_adj = []
+    for i in range(len(f_list)):
+        if f_list[i] in ['', 'None', 'none', 'NONE']:
+            af_adj.append(max(af_adj[:i]))
+        else:
+            af_adj.append(float(f_list[i]))
+
+    est_ct, est_acc_ct = get_est_triangle(ct, acc_ct, ct_f)
+
+    est_fee_triangle, est_acc_fee_triangle = get_est_fee_triangle(ft, est_ct, af_adj)
+
+    fee_ibnr = get_ibnr(est_acc_fee_triangle)
+    count_ibnr = get_ibnr(est_acc_ct)
+
+    ret_count_ibnr = DataFrame([count_ibnr], columns=range(len(count_ibnr)),
+                               index=['件数']).to_html(header="true", table_id="ret_count_ibnr",
+                                                     classes='table table-condensed', float_format='%.4f')
+    ret_fee_ibnr = DataFrame([fee_ibnr], columns=range(len(fee_ibnr)),
+                             index=['费用']).to_html(header="true", table_id="ret_fee_ibnr",
+                                                   classes='table table-condensed', float_format='%.4f')
+    ret_est_fee_triangle = est_fee_triangle.to_html(header="true", table_id="ret_est_fee_triangle",
+                                                    classes='table table-condensed', float_format='%.0f')
+    ret_est_acc_fee_trianglee = est_acc_fee_triangle.to_html(header="true", table_id="ret_est_fee_triangle",
+                                                             classes='table table-condensed', float_format='%.0f')
+
+    tables = [ret_count_ibnr, ret_fee_ibnr, ret_est_fee_triangle, ret_est_acc_fee_trianglee]
+    return tables
+
+
+# basic function
 
 def get_triangle(df, index, columns, values, aggfunc):
     '''
@@ -130,26 +315,19 @@ def get_est_triangle(triangle, acc_triangle, factor):
     return est_triangle, est_acc_triangle
 
 
-#
-# def get_average_fee(average_fee_triangle,std_fee,replace_start,replace_min,fee_month_num=None):
-#     fee=[]
-#     fee_adjust=[]
-#     if fee_month_num is None:
-#         fee=average_fee_triangle.mean().values.tolist()
-#     else:
-#         for i in range(0, average_fee_triangle.shape[1] - fee_month_num):
-#             s= acc_triangle.iloc[len(acc_triangle) - i - factor_month_num:len(acc_triangle) - i, i].mean()
-#             fee.append(s)
-#
-#     for i in range(0,average_fee_triangle.shape[1]):
-#         if i<min(replace_start, average_fee_triangle.shape[1] - replace_min-1):
-#             fee_adjust.append(fee[i])
-#         elif i<len(std_fee):
-#             fee_adjust.append(std_fee[i])
-#         else:
-#             fee_adjust.append(std_fee[-1])
-#     fee+=[fee[-1] for _ in range(len(fee_adjust)-len(fee))]
-#     return fee,fee_adjust
+def get_average_fee(count_triangle, fee_triangle, fee_month_num=None):
+    average_fee_triangle = fee_triangle / count_triangle
+    average_fee = []
+    if fee_month_num is None:
+        average_fee = average_fee_triangle.mean().values.tolist()
+    else:
+        for i in range(0, average_fee_triangle.shape[1] - fee_month_num):
+            s = average_fee_triangle.iloc[len(average_fee_triangle) - i - fee_month_num:len(average_fee_triangle) - i,
+                i].mean()
+            average_fee.append(s)
+
+    average_fee += [None for _ in range(fee_month_num)]
+    return average_fee, average_fee_triangle
 
 
 def get_est_fee_triangle(fee_triangle, est_count_triangle, fee):
